f53c43cea5a5993763334c74c716ad5bba58373c
Phil Neujahr <phil.neujahr@campus.tu-berlin.de>
2021-05-22 17:24:20 +0000

    Changed type of playerontop

--- src/evakuierung/util/tilePiece.ts
+++ src/evakuierung/util/tilePiece.ts
@@ -1,18 +1,26 @@
-export class Player{
-
+// Class for each tile. There will be 13 * 7 tiles in total with each of them having
+// their probabilities (for each direction) hard coded.
+import { Player } from "../util/player"
+export class TilePiece{
     public x: number;
     public y: number;
-    public isQueen: boolean;
+    public upProbability: number;
+    public downProbability: number;
+    public leftProbability: number;
+    public rightProbability: number;
+    private _playersOnTop: Player[] = [];   // this is where the players on top of a tile are stored, it should be a list of objects, when the piglet class is created
 
-    constructor(x: number, y: number, isQueen:boolean){
+    constructor(x: number, y: number, upProbability: number, downProbability: number, leftProbability: number, rightProbability: number) {
         this.x = x;
         this.y = y;
-        this.isQueen = isQueen;
+        this.upProbability = upProbability;
+        this.downProbability = downProbability;
+        this.leftProbability = leftProbability;
+        this.rightProbability = rightProbability;
     }
 
-    public updateCoordinates(x:number, y: number): void {
-        this.x = x;
-        this.y = y;
-    } 
-
+    // can be accessed with dot notation
+    public get playersOnTop() {
+        return this._playersOnTop;
+    }
 }
