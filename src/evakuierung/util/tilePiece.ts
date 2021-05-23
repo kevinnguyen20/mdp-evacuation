b28f2fc1f805bb1702f15e445af7dc48aaf4c3cc
Phil Neujahr <phil.neujahr@campus.tu-berlin.de>
2021-05-23 06:28:24 +0000

    added boolean wall,action,goal to dertermine if tile is a wall, actionfield or goal

--- src/evakuierung/util/tilePiece.ts
+++ src/evakuierung/util/tilePiece.ts
@@ -9,14 +9,20 @@ export class TilePiece{
     public leftProbability: number;
     public rightProbability: number;
     private _playersOnTop: Player[] = [];   // this is where the players on top of a tile are stored, it should be a list of objects, when the piglet class is created
+    public wall: boolean; //is the Tile a Wall?
+    public action: boolean; // is the Tile a actionField?
+    public goal: boolean; // ist the Tile the goal?
 
-    constructor(x: number, y: number, upProbability: number, downProbability: number, leftProbability: number, rightProbability: number) {
+    constructor(x: number, y: number, upProbability: number, downProbability: number, leftProbability: number, rightProbability: number, wall: boolean, action: boolean, goal: boolean ) {
         this.x = x;
         this.y = y;
         this.upProbability = upProbability;
         this.downProbability = downProbability;
         this.leftProbability = leftProbability;
         this.rightProbability = rightProbability;
+        this.wall = wall;
+        this.action = action;
+        this.goal = goal;
     }
 
     // can be accessed with dot notation
