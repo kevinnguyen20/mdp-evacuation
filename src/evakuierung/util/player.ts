2102017a11681034a98d48c656d9eeead8c27f8d
georgik1609 <georgik1609@campus.tu-berlin.de>
2021-05-23 18:37:26 +0200

    Fixed lint error

--- src/evakuierung/util/player.ts
+++ src/evakuierung/util/player.ts
@@ -19,7 +19,7 @@ export class Player{
      * Decides if a player should follow the queen
      * @returns True if the player shold go with the queen or else false
      */
-     private followQueen():boolean{
+    public followQueen():boolean {
         const randomNum: number = Math.random();
         if (randomNum < 0.98){
             return true;
