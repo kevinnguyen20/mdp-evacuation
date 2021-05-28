52f42a867fb0732d258c5383800ec1ed193672b4
kevinnguyen <k.nguyen.2@campus.tu-berlin.de>
2021-05-28 23:34:51 +0200

    add printtilepiece

--- src/evakuierung/util/debugClass.ts
+++ src/evakuierung/util/debugClass.ts
@@ -3,44 +3,54 @@ import { TileParser } from "./tileParser";
 import { TilePiece } from "./tilePiece";
 import { LevelFunctions } from "./levelFunctions";
 
-type Coordination = {
+type Coordinates = {
     x: number;
     y: number;
 };
 
+type Probabilities = {
+    up: number;
+    right: number;
+    down: number;
+    left: number;
+    followQueen: number;
+};
+
+type TileType = {
+    wall: boolean;
+    action: boolean;
+    goal: boolean;
+};
+
 type debugFigure = {
-    spawnCoordinates: Coordination;
-    coordinates: Coordination;
+    coordinates: Coordinates;
     isQueen: boolean;
     image: Phaser.GameObjects.Image;
 };
 
 type debugTilePiece = {
-    coordinate
-}
+    coordinates: Coordinates;
+    probabilites: Probabilities;
+    tileType: TileType;
+    playersOnTop: number;
+};
 
 export class DebugClass {
 
-    /**
-     * figure can be "null" or "undefinded". Data type "null"
-     * belongs to data type "object".
-     * @param figure the movable object on the map
-     * @returns 
-     */
-    public static checkFigureNull(figure: Figure | null | undefined): boolean {
-        if(figure === undefined) {
-            console.log("[-] <figure> ist undefinded");
+    public static checkNull(object: Figure | TilePiece | null | undefined): boolean {
+        if(object === undefined) {
+            console.log("[-] object is undefinded");
             return false;
         }
-        else if(figure === null) {
-            console.log("[-] <figure> ist null");
+        else if(object === null) {
+            console.log("[-] object is null");
             return false;
         }
-        else if(typeof figure === "object")
+        else if(typeof object === "object")
             return true;
 
         else {
-            console.log("[-] Unexpected error occured in <figure>");
+            console.log("[-] Unexpected error occured in checkNull");
             return false;
         }
     }
@@ -48,32 +58,23 @@ export class DebugClass {
 
     /*
      * 
-     * Debug information for <figure>
+     * Debug information about <figure>
      */
 
-    public static printSpawnPosition(figure: Figure | null | undefined): void {
-        if(this.checkFigureNull(figure)) {
-            console.log("[-] Fehler bei Debug-Info über <figure>");
-            process.exit(1);
-        }
+    public static isQueen(dFigure: debugFigure): void {
+        if(dFigure.isQueen === true) 
+            console.log("Königin: -----------------------")
+        else if(dFigure.isQueen === false)
+            console.log("Untertan: -----------------------");
         else {
-            const dFigure: debugFigure = null;
-            dFigure.spawnCoordinates.x = figure.x;
-            dFigure.spawnCoordinates.y = figure.y;
-            
-            if(dFigure.isQueen === true) 
-                console.log("Königin: -----------------------")
-            else if(dFigure.isQueen === false)
-                console.log("Untertan: -----------------------");
-
-            console.log(`Koordinaten: [${dFigure.spawnCoordinates.x}, ${dFigure.spawnCoordinates.y}]`);
+            console.log("[-] Unexpected error occured in <figure> isQueen")
         }
     }
 
     public static printFigure(figure: Figure | null | undefined): void {
 
-        if(this.checkFigureNull(figure)) {
-            console.log("[-] Fehler bei Debug-Info über <figure>");
+        if(this.checkNull(figure)) {
+            console.log("[-] Error in <figure>");
             process.exit(1);
         } 
         else {
@@ -84,30 +85,44 @@ export class DebugClass {
             dFigure.image = figure.image;
 
 
-            if(dFigure.isQueen === true) 
-                console.log("Königin: -----------------------")
-            else if(dFigure.isQueen === false)
-                console.log("Untertan: -----------------------");
-            else {
-                console.log("[-] Fehler bei <figure> isQueen");
-                process.exit(1);
-            }
+            this.isQueen(dFigure);
 
             console.log(`Koordinaten: [${dFigure.coordinates.x}, ${dFigure.coordinates.y}]`);
-            console.log(dFigure.image);
         }
     }
 
-    /* Debug information about <tilePiece>
+    /* 
+     * Debug information about <tilePiece>
+     *
      */
 
     public static printTilePiece(tilePiece: TilePiece | null | undefined): void {
-        if(this.checkFigureNull) {
-            console.log("[-] Fehler bei Debug-Info über <tilePiece>");
+        if(this.checkNull(tilePiece)) {
+            console.log("[-] Error in <tilePiece>");
             process.exit(1);
         }
         else {
+            const dTilePiece: debugTilePiece = null;
+            dTilePiece.coordinates.x = tilePiece.tileCoordinates[0];
+            dTilePiece.coordinates.y = tilePiece.tileCoordinates[1];
+            dTilePiece.probabilites.up = tilePiece.directionProbabilities[0];
+            dTilePiece.probabilites.right = tilePiece.directionProbabilities[1];
+            dTilePiece.probabilites.down = tilePiece.directionProbabilities[2];
+            dTilePiece.probabilites.left = tilePiece.directionProbabilities[3];
+            dTilePiece.tileType.wall = tilePiece.tileType[0];
+            dTilePiece.tileType.action = tilePiece.tileType[1];
+            dTilePiece.tileType.goal = tilePiece.tileType[2];
+            dTilePiece.playersOnTop = tilePiece.playersOnTop;
 
+            console.log(`Koordinaten:           [${dTilePiece.coordinates.x}, ${dTilePiece.coordinates.y}]`);
+            console.log(`Wahrscheinlichkeiten:`); 
+            console.log(`Norden|  Osten|  Süden| Westen`);
+            console.log(`------+-------+-------+-------`);
+            console.log(`     ${dTilePiece.probabilites.up}|  ${dTilePiece.probabilites.right}|  ${dTilePiece.probabilites.down}| ${dTilePiece.probabilites.left}`);
+            console.log(`Wall:                  ${dTilePiece.tileType.wall}`);
+            console.log(`Action:                ${dTilePiece.tileType.action}`);
+            console.log(`Goal:                  ${dTilePiece.tileType.goal}`);
+            console.log(`Objekte auf dem Feld:  ${dTilePiece.playersOnTop}`);
         }
     }
 }
