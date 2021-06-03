8e2204ba509ccadc4441f2cb1a743067c773ebc7
Krutarth Parwal <k.parwal@campus.tu-berlin.de>
2021-06-03 11:51:00 +0200

    added a new scene LevelMenu.ts + changes made in game.ts+ button functionsadded+Back button also implemented to come back to main menu from level menu

--- src/evakuierung/game.ts
+++ src/evakuierung/game.ts
@@ -2,6 +2,7 @@ import "phaser";
 import { AUTO, Scale } from "phaser";
 
 import { MainMenu } from "./scenes/MainMenu";
+import {LevelMenu} from "./scenes/LevelMenu";
 import {level1} from "./scenes/level1";
 import {level2} from "./scenes/level2";
 import {level3} from "./scenes/level3";
@@ -14,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
         height: 600,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: [MainMenu, level1, level2, level3],
+    scene: [MainMenu, LevelMenu, level1, level2, level3],
     physics: {
         default: 'arcade',
         arcade: {
