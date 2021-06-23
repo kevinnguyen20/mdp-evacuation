13d3ab39af7f27913c0b61c1270bf3b62b950e58
Phil Neujahr <phil.neujahr@campus.tu-berlin.de>
2021-06-23 13:11:54 +0000

    added difficulty button and logic behind it

--- src/evakuierung/scenes/MainMenu.ts
+++ src/evakuierung/scenes/MainMenu.ts
@@ -20,12 +20,17 @@ export class MainMenu extends Phaser.Scene{
         this.load.image('level2_button','./assets/sprites/level2_button.png');
         this.load.image('level3_button','./assets/sprites/level3_button.png');
         this.load.image('back_button','./assets/sprites/back_button.png');
+        this.load.image('difficulty', './assets/sprites/difficulty_button.png' );
+        this.load.image('easy', './assets/sprites/easy_button.png' );
+        this.load.image('medium', './assets/sprites/medium_button.png' );
+        this.load.image('hard', './assets/sprites/hard_button.png' );
         
       
     }
 
 
     create(): void{
+        let diff= 10;
         const text1 = this.add.text(130, 100,'EVAKUIERUNG', {font : " 74px Arial Black",}).setDepth(1);
         text1.setFill('#00f');
         text1.setColor('#00f')
@@ -50,10 +55,12 @@ export class MainMenu extends Phaser.Scene{
         // PLAY BUTTON
         playButton.setInteractive()
         playButton.on("pointerdown",()=>{
-            this.scene.transition({
+          /*  this.scene.transition({
                 target: "level1",
                 duration: 10
-            });
+            });*/
+            this.scene.start('level1', {diff: diff});
+
         });
         playButton.on('pointerover', function(){playButton.setScale(0.85, 0.85)});
         playButton.on('pointerout', function(){ playButton.setScale(1, 1)});
@@ -61,13 +68,13 @@ export class MainMenu extends Phaser.Scene{
         // level 1 button
         level1button.setInteractive();
 
-
         level1button.on("pointerdown",()=>{
             
-            this.scene.transition({
-                target: "level1",
-                duration: 10
-            });
+            //this.scene.transition({
+           //     target: "level1",
+          //      duration: 10
+          //  });
+          this.scene.start('level1', {diff: diff});
         });
 
         level1button.on('pointerover', function(pointer){
@@ -85,10 +92,13 @@ export class MainMenu extends Phaser.Scene{
 
         level2button.on("pointerdown",()=>{
             
-            this.scene.transition({
+          /*  this.scene.transition({
                 target: "level2",
                 duration: 10
             });
+            */
+            this.scene.start('level2', {diff: diff});
+
         });
 
         level2button.on('pointerover', function(pointer){
@@ -106,10 +116,13 @@ export class MainMenu extends Phaser.Scene{
 
         level3button.on("pointerdown",()=>{
             
-            this.scene.transition({
+           /* this.scene.transition({
                 target: "level3",
-                duration: 10
-            });
+                duration: 10,
+
+            });*/
+            this.scene.start('level3', {diff: diff});
+
         });
 
         level3button.on('pointerover', function(pointer){
@@ -119,6 +132,85 @@ export class MainMenu extends Phaser.Scene{
             level3button.setScale(1, 1);
         });
 
+        const difficulty = this.add.image(this.game.renderer.width/2, this.game.renderer.height/2+250,'difficulty').setDepth(1);
+        const easy = this.add.image(this.game.renderer.width/2-200, this.game.renderer.height/2+250,'easy').setDepth(1);
+        const medium = this.add.image(this.game.renderer.width/2, this.game.renderer.height/2+250,'medium').setDepth(1);
+        const hard = this.add.image(this.game.renderer.width/2+200, this.game.renderer.height/2+250,'hard').setDepth(1);
+        easy.setVisible(false);
+        medium.setVisible(false);
+        hard.setVisible(false);
+        const difficultytxt = this.add.text(this.game.renderer.width/2- 110, this.game.renderer.height/2+200, 'Current difficulty: easy')
+
+
+
+        difficulty.setInteractive();
+        easy.setInteractive();
+        medium.setInteractive();
+        hard.setInteractive();
+
+        difficulty.on('pointerover', function(pointer){
+            difficulty.setScale(0.85, 0.85);
+        });
+        difficulty.on('pointerout', function(pointer){
+            difficulty.setScale(1, 1);
+        });
+
+        easy.on('pointerover', function(pointer){
+            easy.setScale(0.85, 0.85);
+        });
+        easy.on('pointerout', function(pointer){
+            easy.setScale(1, 1);
+        });
+
+        medium.on('pointerover', function(pointer){
+            medium.setScale(0.85, 0.85);
+        });
+        medium.on('pointerout', function(pointer){
+            medium.setScale(1, 1);
+        });
+
+        hard.on('pointerover', function(pointer){
+            hard.setScale(0.85, 0.85);
+        });
+        hard.on('pointerout', function(pointer){
+            hard.setScale(1, 1);
+        });
+        difficulty.on('pointerup', () => {
+            difficulty.setVisible(false);
+            easy.setVisible(true);
+            medium.setVisible(true);
+            hard.setVisible(true);
+        })
+
+        easy.on('pointerup', () => {
+            diff = 10;
+            difficulty.setVisible(true);
+            easy.setVisible(false);
+            medium.setVisible(false);
+            hard.setVisible(false);
+            difficultytxt.setText('Current difficulty: easy')
+        })
+
+        
+        medium.on('pointerup', () => {
+            diff = 20;
+            difficulty.setVisible(true);
+            easy.setVisible(false);
+            medium.setVisible(false);
+            hard.setVisible(false);
+            difficultytxt.setText('Current difficulty: medium')
+        })
+
+        
+        hard.on('pointerup', () => {
+            diff = 30;
+            difficulty.setVisible(true);
+            easy.setVisible(false);
+            medium.setVisible(false);
+            hard.setVisible(false);
+            difficultytxt.setText('Current difficulty: hard')
+        })
+        
 
         
     }
